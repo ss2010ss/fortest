d7831f09ed89615c7ac63d616e3e07e75c0060fb
ss2010ss <ss2010@ya.ru>
2025-07-15 08:10:52 +0200

    Creat new sql base

--- fortest/helloapp/models.py
+++ fortest/helloapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class textsfortest(models.Model):
+    text = models.CharField(max_length=1000, default='')
+
+    parenttext = models.CharField(max_length=200, default='')
+
+    count = models.IntegerField(default=0)
+
+    like = models.IntegerField(default=0)
+
+    dislike = models.IntegerField(default=0)
+
+    weight = models.IntegerField(default=0)
+
+  
